b6517f1a9e58cab6208f55336f81d5d14a3dfe07
A-Kumada <105118306+A-Kumada@users.noreply.github.com>
2022-06-27 12:14:03 +0000

    1回目の提出

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+  get "/home/about" => "homes#about", as: "about"
+  resources :users, only: [:index, :show, :edit, :update, :create]
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update]
+end
